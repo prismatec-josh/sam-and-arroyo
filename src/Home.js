2cc53aa3c15874b6ac305efec9c14f1156254cb0
Delta <josh@prismatec.co>
2021-10-18 23:01:25 -1000

    Improve styling

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="Home">
         <h1>
           Sam Arroyo is a film photographer capturing honest moments of life,
-          mamas and the ocean.
+          mamas, and the ocean.
         </h1>
       </div>
     </main>
